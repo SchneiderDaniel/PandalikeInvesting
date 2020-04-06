fa4fcce375919a3875f45dde658b11161c053143
Daniel Schneider <daniel.schneider.privat@googlemail.com>
2020-04-06 18:54:19 +0200

    First draft of the index page

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from datetime import datetime
+import os
+
 
 app = Flask(__name__)
 
@@ -7,6 +9,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'resources/img/favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == "__main__":
     app.run(debug=True)
