b18ad5b936b1cced81538b4ab9b8e4ff0e55aab1
Daniel Schneider <daniel.schneider.privat@googlemail.com>
2020-04-08 20:43:17 +0200

    New Navtab demo

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from datetime import datetime
 import os
-
+import sys
+from forms import ContactForm
+from flask_bootstrap import Bootstrap
+from flask_fontawesome import FontAwesome
 
 app = Flask(__name__)
+fa = FontAwesome(app)
+Bootstrap(app)
+
+# app.config.from_mapping(
+#     SECRET_KEY=b'\xd6\x04\xbdj\xfe\xed$c\x1e@\xad\x0f\x13,@G')
+# app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route('/')
 def index():
@@ -13,10 +22,37 @@ def index():
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'resources/img/favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/visu')
-def visualization():
-    return render_template('visu.html')
 
+@app.route('/visu/', methods=['GET', 'POST'])
+def visualization():
+    if request.method == 'POST':
+        # tickerID = request.form["tickerInput"]
+        #print(request.form, file=sys.stderr)
+        return render_template('visu.html')
+    else:
+        return render_template('visu.html')
+
+@app.route('/contact', methods=('GET', 'POST'))
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        print('POSTI!', file=sys.stderr)
+        print(form.name, file=sys.stderr)
+        print(form.email, file=sys.stderr)
+        print(form.body, file=sys.stderr)
+        return redirect(url_for('success'))
+    else:
+        print('Get!', file=sys.stderr)
+        return render_template('contact.html', form=form)
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+#  print('POSTI!', file=sys.stderr)
