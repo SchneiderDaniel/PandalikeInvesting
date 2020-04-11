4dc3843650cc281568c896f108af096ce130f5e2
Daniel Schneider <daniel.schneider.privat@googlemail.com>
2020-04-11 20:35:45 +0200

    new main and account html

--- homepage/routes.py
+++ homepage/routes.py
@@ -95,4 +95,5 @@ def logout():
 @app.route('/account')
 @login_required
 def account():
-    return render_template('account.html', title = 'Account')
+    image_file = url_for('static', filename='resources/img/profile_pics/' + current_user.image_file )
+    return render_template('account.html', title = 'Account', image_file = image_file)
