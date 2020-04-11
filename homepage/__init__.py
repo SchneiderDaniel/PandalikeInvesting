ef3b76ff3f5e73fcabb00ac9ce0929a7ea53c0ca
Daniel Schneider <daniel.schneider.privat@googlemail.com>
2020-04-11 19:51:56 +0200

    added a sidebar and removed flask-bootstrap4

--- homepage/__init__.py
+++ homepage/__init__.py
@@ -1,6 +1,5 @@
 #Libs
 from flask import Flask
-from flask_bootstrap import Bootstrap
 from flask_fontawesome import FontAwesome
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -15,7 +14,6 @@ RECAPTCHA_PUBLIC_KEY = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
 RECAPTCHA_PRIVATE_KEY = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
 app = Flask(__name__)
 fa = FontAwesome(app)
-Bootstrap(app)
 # DEV (Don't use for PROD)
 app.config.from_mapping(SECRET_KEY=b'\xd6\x04\xbdj\xfe\xed$c\x1e@\xad\x0f\x13,@G')
 # app.config.from_mapping(SECRET_KEY=os.getenv('SECRET_PUBLIC_KEY'))
