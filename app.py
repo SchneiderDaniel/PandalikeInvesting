36793bcde420b00b87292b3f02d8c4c0fe3941ec
Daniel Schneider <daniel.schneider.privat@googlemail.com>
2020-04-11 13:23:31 +0200

    Dev DB setup

--- app.py
+++ app.py
@@ -1,25 +1,54 @@
+#Libs
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory, flash
 from datetime import datetime
 import os
 import sys
-from forms import ContactForm, BT_GeneralForm, RegistrationForm, LoginForm
 from flask_bootstrap import Bootstrap
 from flask_fontawesome import FontAwesome
+from flask_sqlalchemy import SQLAlchemy
+
+#Code
+from forms import ContactForm, BT_GeneralForm, RegistrationForm, LoginForm
+
 
 
 # RECAPTCHA_PUBLIC_KEY = os.getenv('RECAPTCHA_PUBLIC_KEY')
 # RECAPTCHA_PRIVATE_KEY = os.getenv('RECAPTCHA_PRIVATE_KEY')
+# DEV (Don't use for PROD)
 RECAPTCHA_PUBLIC_KEY = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
 RECAPTCHA_PRIVATE_KEY = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
-
-
 app = Flask(__name__)
 fa = FontAwesome(app)
 Bootstrap(app)
-
+# DEV (Don't use for PROD)
 app.config.from_mapping(SECRET_KEY=b'\xd6\x04\xbdj\xfe\xed$c\x1e@\xad\x0f\x13,@G')
 # app.config.from_mapping(SECRET_KEY=os.getenv('SECRET_PUBLIC_KEY'))
 app.config.from_object(__name__)
+# DEV (Don't use for PROD)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User (db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), unique = True, nullable = False)
+    email = db.Column(db.String(120), unique = True, nullable = False)
+    image_file = db.Column(db.String(20), nullable = False, default = 'default.jpg')
+    password = db.Column(db.String(60), nullable = False)
+    posts = db.relationship('Post', backref = 'author', lazy = True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}' )"
+
+class Post  (db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    content = db.Column(db.Text, nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+
+    def __repr__(self):
+        return f"User('{self.title}', '{self.date_posted}' )"
+
 
 
 @app.route('/')
