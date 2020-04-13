a5ad2c429e832b3444219141c7524a5a77293425
Daniel Schneider <daniel.schneider.privat@googlemail.com>
2020-04-13 18:48:46 +0200

    error handling

--- homepage/__init__.py
+++ homepage/__init__.py
@@ -21,10 +21,12 @@ def create_app(config_class=Config):
     from homepage.users.routes import users
     from homepage.posts.routes import posts
     from homepage.main.routes import main
+    from homepage.errors.handlers import errors
 
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     fa.init_app(app)
     db.init_app(app)
