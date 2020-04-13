5152afb48d326f1f225d9a6c8677df3b0f5b12ca
Daniel Schneider <daniel.schneider.privat@googlemail.com>
2020-04-13 13:43:17 +0200

    pagination rdy

--- homepage/routes.py
+++ homepage/routes.py
@@ -16,9 +16,10 @@ def index():
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'resources/img/favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/blog', methods=['GET', 'POST'])
+@app.route('/blog')
 def blog():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page = page, per_page = 5)
     return render_template('blog.html', title = 'Blog', posts=posts)
 
 @app.route('/terms')
@@ -172,3 +173,12 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted!', 'success')
     return (redirect(url_for('blog', post_id = post.id)))
+
+@app.route('/user/<string:username>')
+def user(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404();
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page = page, per_page = 5)
+    return render_template('user.html', title = 'Blog', posts=posts, user=user)
