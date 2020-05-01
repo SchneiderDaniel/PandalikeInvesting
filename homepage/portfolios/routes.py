b8cc20f0f9f2c01d182c3fbb04bd05449b60533e
Daniel Schneider <daniel.schneider.privat@googlemail.com>
2020-05-01 10:01:31 +0200

    added html pages

--- homepage/portfolios/routes.py
+++ homepage/portfolios/routes.py
@@ -10,3 +10,13 @@ portfolios = Blueprint('portfolios', __name__)
 @portfolios.route('/portfolio/home')
 def portfolio():
     return render_template('portfolio.html', title='Pandalike Investing - Portfolio')
+
+
+@portfolios.route('/portfolio/add_position')
+def addPosition():
+    return render_template('portfolio_addPostion.html', title='Pandalike Investing - Portfolio Add Position')
+
+
+@portfolios.route('/portfolio/overview')
+def overvie():
+    return render_template('portfolio_Overview.html', title='Pandalike Investing - Portfolio Overview')
