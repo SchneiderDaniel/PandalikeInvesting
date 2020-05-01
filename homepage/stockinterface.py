22b0d53bd02f1d7d8c490b15283babe072a9df31
Daniel Schneider <daniel.schneider.privat@googlemail.com>
2020-05-01 18:14:02 +0200

    dummy functions for stockinterface

--- homepage/stockinterface.py
+++ homepage/stockinterface.py
@@ -12,7 +12,6 @@ def isTickerValid(ticker):
     
 
 def getNameToTicker(ticker):
-
     companyName = ""
     try:
         aTicker = yf.Ticker(ticker)
@@ -26,3 +25,16 @@ def getNameToTicker(ticker):
         except:
             companyName = " Name not found"
     return companyName
+
+
+def getCorrelationDiagram(ticker1, ticker2):
+    
+    result = [['2017-01-01',0.1],['2018-01-01',0.8],['2018-01-01',0.5] ]
+    
+    return result
+
+def getCorrelationMatrix(tickers):
+    
+    result = [[1.0,0.1,0.25],[0.3,1.0,0.2],[0,6,0.5,1.0] ]
+    
+    return result
