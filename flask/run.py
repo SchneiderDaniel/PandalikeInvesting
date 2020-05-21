595391f57e7bc0c664d7edf3143fdc8ef8bef50b
SchneiderDaniel <daniel.schneider.privat@googlemail.com>
2020-05-21 19:03:10 +0200

    postgres und adminer as docker added

--- flask/run.py
+++ flask/run.py
@@ -3,7 +3,7 @@ from homepage import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True, threaded=True)
+    app.run(port=5001, debug=True, threaded=True)
 
 
 #Cheats
